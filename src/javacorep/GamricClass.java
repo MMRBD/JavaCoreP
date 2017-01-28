34142fe8818b3fb8c0af5ddc15738c85d8026bbb
mokle <mokle@DESKTOP-OF6056D>
2017-01-27 17:16:10 -0800

    This is new GUI Project

--- src/javacorep/GamricClass.java
+++ src/javacorep/GamricClass.java
@@ -4,7 +4,26 @@ package javacorep;
  *
  * @author mokle
  */
-public class GamricClass {
+public class GamricClass<T> {
 
-    
+    private T t;
+
+    public void add(T t) {
+        this.t = t;
+    }
+
+    public T getT() {
+        return t;
+    }
+    public static void main(String[] args) {
+        GamricClass <String> stringValue = new GamricClass();
+        GamricClass <Integer> integerValue = new GamricClass();
+        stringValue.add("HHi I am String");
+        integerValue.add(120);
+        
+        System.out.println(stringValue.getT());
+        System.out.println(integerValue.getT());
+                
+    }
+
 }
